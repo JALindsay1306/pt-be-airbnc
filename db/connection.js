3b0c4328513525fd93911aaf9510b3dbcd6caa74
JALindsay1306 <jalindsay89@gmail.com>
2025-01-20 20:00:33 +0000

    fix in connection to avoid SSL errors

--- db/connection.js
+++ db/connection.js
@@ -8,8 +8,9 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = { rejectUnauthorized: false };
 } 
-config.ssl = { rejectUnauthorized: false };
+
 
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
